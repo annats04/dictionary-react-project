8ad358a284dccc11d10cb7a42cb9e68bbb40b78e
annats04 <ann_tsant@hotmail.com>
2023-05-12 12:12:46 +0200

    set up React to build the dictionary project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>Hello</header>
+      <p>This will be a dictionary project</p>
+    </div>
+  );
+}
+
+export default App;
