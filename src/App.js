b46aa180273aba001aa66c1c19d7c32926039c9a
annats04 <ann_tsant@hotmail.com>
2023-05-12 19:09:44 +0200

    built search form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header>Hello</header>
-      <p>This will be a dictionary project</p>
+      <div className="container">
+        <header className="App-header">DICTIONARY PROJECT</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center App-footer">
+          <small>Coded by Anna Tsantekidou</small>
+        </footer>
+      </div>{" "}
     </div>
   );
 }
