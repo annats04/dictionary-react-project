25d610687fd9824e2808a6a29eaa26bfdc276a16
annats04 <ann_tsant@hotmail.com>
2023-05-17 14:57:43 +0200

    created Meanings component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data.meanings[0]);
+    console.log(response.data);
+    console.log(response.data.meanings[0].definition);
+    setResults(response.data);
   }
 
   function search(event) {
@@ -25,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Meaning.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Synonyms from "./Synonyms";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      <div>
+        <div className="definition">{props.meaning.definition}</div>
+        <div className="example">
+          <em>{props.meaning.example}</em>
+        </div>
+        <div className="synonyms">
+          <strong> Synonyms:</strong>
+          <Synonyms synonyms={props.meaning.synonyms} />
+        </div>
+      </div>
+    </div>
+  );
+}
